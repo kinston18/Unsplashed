cbd51e67f85a1bb78b654401a6a38fc93a5525fb
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-02-19 16:59:22 +0530

    unsplashed made

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 
 function App() {
   const[value,setValue]=useState("")
+  const[results,setResults]=useState([])
+  //7ejDlQXavbbfuqk7UUCeKQhwwv1TVGWc2TMMlc1jQlU
+
+  const fetchImages=()=>{
+    fetch(`https://api.unsplash.com/search/photos?client_id=7ejDlQXavbbfuqk7UUCeKQhwwv1TVGWc2TMMlc1jQlU&query=${value}&orientation=squarish`).then(res=>res.json())
+      .then(data=>{
+    console.log(data)
+        setResults(data.results)
+    })
+  }
   return (
     <div className="App">
       <div className="mydiv">
@@ -12,7 +22,15 @@ function App() {
         type="text"
         value={value} 
         onChange={(e)=>setValue(e.target.value)} />
-        <button>send</button>
+        <button onClick={()=>fetchImages()}>send</button>
+      </div>
+
+      <div className="gallery">
+        {
+          results.map((item)=>{
+            return <img className="item" key={item.id} src={item.urls.regular} />
+          })
+        }
       </div>
     </div>
   );
